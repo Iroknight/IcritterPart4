9bc936fff1513b82a238bf9416a7c3563a48844e
iroknight <iroknight@hotmail.com>
2015-03-08 04:13:21 -0700

    Created Slug ICritter

---
+++ ICritterPart4/src/model/ICritterSlug.java
@@ -0,0 +1,17 @@
+package model;
+
+/**
+ * Defines a Slug ICritter.
+ * 
+ * @author Andy
+ *
+ */
+
+public class ICritterSlug extends LandICritter {
+
+	public ICritterSlug(String theName, Owner theOwner) {
+		super(theName, theOwner);
+		// Auto-generated constructor stub
+	}
+
+}
